8f0e55f5d7ff4dfc95a0396ca1f0f57c1b15115d
AbdAlmug <abdallahalmog@gmail.com>
2025-09-15 21:17:48 +0300

    Upload the task to github

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,33 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function color() {
+    document.getElementById("Dark").style.backgroundColor = "black";
+}
+function FN() {
+    alert("Enter Your First Name");
+}
+function LN() {
+    alert("Enter Your Last Name");
+}
+function EA() {
+    alert("Enter Your Email Address");
+}
+function PW() {
+    alert("Enter Your Password");
+}
+function CY() {
+    alert("Enter Your City");
+}
+function GR() {
+    alert("Select Your Gender, If you want");
+}
+function SD() {
+    alert("Select What Degree Do You Have");
+}
+function BA() {
+    alert("Selecting Bachelor\'s Degree ");
+}
+function MA() {
+    alert("Selecting Master\'s Degree ");
+}
+function PhD() {
+    alert("Selecting PhD\'s Degree ");
+}
